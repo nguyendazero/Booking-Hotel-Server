770a9d12793184435a4278e2ef3db32cec95b985
Nguyen Da Da <nguyendazero@gmail.com>
2025-03-03 17:25:18 +0700

    move config cloudinary to application.properties

--- src/main/java/com/vinova/booking_hotel/authentication/config/CloudinaryConfig.java
+++ src/main/java/com/vinova/booking_hotel/authentication/config/CloudinaryConfig.java
@@ -2,19 +2,32 @@ package com.vinova.booking_hotel.authentication.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
+    @Value("${cloudinary.secure}")
+    private boolean secure;
+
     @Bean
     public Cloudinary getCloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dzihbbinr",
-                "api_key", "424214716917593",
-                "api_secret", "TaJ7wj3NFhFB_RV8CkBsXERYJiM",
-                "secure", true
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", secure
         ));
     }
 }
