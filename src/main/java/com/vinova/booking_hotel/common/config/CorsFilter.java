1717f3fa5f83e70fb848fbd601653328712b46df
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-28 11:40:18 +0700

    fix github login

--- src/main/java/com/vinova/booking_hotel/common/config/CorsFilter.java
+++ src/main/java/com/vinova/booking_hotel/common/config/CorsFilter.java
@@ -5,23 +5,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
 
 @Component
 public class CorsFilter implements Filter {
 
-    private final Set<String> allowedOrigins = Set.of("https://nguyendazero.github.io");
-
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
         String origin = request.getHeader("Origin");
-        if (allowedOrigins.contains(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", origin); // Chỉ cho phép domain hợp lệ
+
+        if (origin != null && origin.equals("https://nguyendazero.github.io")) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
@@ -33,4 +30,5 @@ public class CorsFilter implements Filter {
             chain.doFilter(req, res);
         }
     }
+
 }
