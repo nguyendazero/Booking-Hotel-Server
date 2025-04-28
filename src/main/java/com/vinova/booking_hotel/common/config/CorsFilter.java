e59ca9f89d5fa36a12b2ec30023d027072848f1a
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-28 08:56:56 +0700

    fix github login

--- src/main/java/com/vinova/booking_hotel/common/config/CorsFilter.java
+++ src/main/java/com/vinova/booking_hotel/common/config/CorsFilter.java
@@ -1,40 +1,34 @@
-//package com.vinova.booking_hotel.common.config;
-//
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.stereotype.Component;
-//import java.io.IOException;
-//import java.util.Arrays;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Component
-//public class CorsFilter implements Filter {
-//
-//    private final Set<String> allowedOrigins = new HashSet<>(Arrays.asList(
-//            "http://localhost:5173",
-//            "https://nguyendazero.github.io"
-//    ));
-//
-//    @Override
-//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-//        HttpServletResponse response = (HttpServletResponse) res;
-//        HttpServletRequest request = (HttpServletRequest) req;
-//
-//        String origin = request.getHeader("Origin");
-//        if (origin != null && allowedOrigins.contains(origin)) {
-//            response.setHeader("Access-Control-Allow-Origin", origin);
-//        }
-//
-//        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//
-//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(req, res);
-//        }
-//    }
-//}
+package com.vinova.booking_hotel.common.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Component
+public class CorsFilter implements Filter {
+
+    private final Set<String> allowedOrigins = new HashSet<>(); // Để trống để kiểm tra sau
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", Objects.requireNonNullElse(origin, "*")); // Cho phép mọi origin gửi đến
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
