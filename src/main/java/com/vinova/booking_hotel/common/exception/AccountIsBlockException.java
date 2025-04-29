4ad53d3cdd279ee1752d297eff841e9f662170ca
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-29 13:33:04 +0700

    zzzzzzzzzzzzz

--- src/main/java/com/vinova/booking_hotel/common/exception/AccountIsBlockException.java
+++ src/main/java/com/vinova/booking_hotel/common/exception/AccountIsBlockException.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class AccountIsBlockException extends RuntimeException {
 
     public AccountIsBlockException(String blockReason) {
-        super("Account is blocked, because: " + blockReason);
+        super("Account is blocked zzzzzzzzzzzzzz, because: " + blockReason);
     }
 }
 
